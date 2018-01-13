6703953c5f525b208c9248889513dcaa2be6ed29
linyitin <lowya0301@gmail.com>
2018-01-13 18:24:48 +0800

    Create DNS_Hinet.py

---
+++ DNS_Hinet.py
@@ -0,0 +1,37 @@
+from socket import *
+import time
+import json
+splunk_host=('163.18.26.108',12345)
+hinet_dns_server='168.95.1.1'
+
+s=socket(AF_INET, SOCK_DGRAM)
+s.settimeout(3)
+
+sk=socket(AF_INET, SOCK_DGRAM)
+sk.connect(splunk_host)
+
+packet = "24 1a 01 00 00 01 00 00 00 00 00 00 03 77  77 77 06 67 6f 6f 67 6c 65 03 63 6f 6d 00 00 01  00 01"
+hex_package = packet.split()
+
+hex_ints = [int(i, 16) for i in hex_package]
+hex_bytes = [chr(i) for i in hex_ints]
+packet = "".join(hex_bytes)
+
+
+while True:
+	TransmissionTime=time.strftime("%Y-%m-%d_%H:%M:%S",time.localtime())
+	start=time.time()
+	try:
+		s.sendto(packet,(hinet_dns_server,53))
+		s.recvfrom(1024)
+		ArrivalTime=time.strftime("%Y-%m-%d_%H:%M:%S",time.localtime())
+		end=time.time()-start
+		ans={"TransmissionTime":TransmissionTime,"ArrivalTime":ArrivalTime,"DnsIp":hinet_dns_server,"time":end}
+		json_str=json.dumps(ans)
+		sk.sendto(json_str,splunk_host)
+		time.sleep(10)
+	except:
+		ans={"TransmissionTime":TransmissionTime,"ArrivalTime":ArrivalTime,"DnsIp":hinet_dns_server,"error":"timeout"}
+		json_str=json.dumps(ans)
+		sk.sendto(json_str,splunk_host)
+		time.sleep(10)
